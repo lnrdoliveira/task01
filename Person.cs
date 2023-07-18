81c115d032406b3b20071ba5dd600c326f4149af
Leonardo <lnrdoliveira.dev@gmail.com>
2023-07-18 16:29:54 -0300

    Primeiro exercício

---
+++ Person.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace task01
+{
+    public class Person
+    {
+        public string Name;
+        public int Age;
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 using System.Globalization;
+using task01;
 
 class Program{
     static void Main(String[] args){
-        System.Console.WriteLine("Dados da primeira pessoa: ");
-        System.Console.WriteLine("Nome: ");
-        System.Console.WriteLine("Idade: ");
+        Person firstPerson, secondPerson;
+        firstPerson = new Person();
+        secondPerson = new Person();
+
+        System.Console.WriteLine("Infos about first person: ");
+        System.Console.Write("Name: ");
+        firstPerson.Name = Console.ReadLine();
+        System.Console.Write("Age: ");
+        firstPerson.Age = int.Parse(Console.ReadLine());
+        System.Console.WriteLine("Infos about second person: ");
+        System.Console.Write("Name: ");
+        secondPerson.Name = Console.ReadLine();
+        System.Console.Write("Age: ");
+        secondPerson.Age = int.Parse(Console.ReadLine());
+
+        if(firstPerson.Age > secondPerson.Age){
+            System.Console.WriteLine("Older person: "+firstPerson.Name);
+        }
+        else if(secondPerson.Age > firstPerson.Age){
+            System.Console.WriteLine("Older person: "+secondPerson.Name);
+        }
+        else {
+            System.Console.WriteLine("Both people are the same age.");
+        }
     }
 }
