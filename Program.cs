ae89d7bbdff525da4668492237b22fecccee5a90
Leonardo <lnrdoliveira.dev@gmail.com>
2023-07-18 16:08:52 -0300

    Primeiro exercício

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Globalization;
+
+class Program{
+    static void Main(String[] args){
+        System.Console.WriteLine("Dados da primeira pessoa: ");
+        System.Console.WriteLine("Nome: ");
+        System.Console.WriteLine("Idade: ");
+    }
+}
